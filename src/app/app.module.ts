3ae9e7f4ba972004ce3d00c0bb8f213a0999f08e
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-30 15:25:39 +0530

    header & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatModule } from './appModules/mat.module';
+import { HeaderComponent } from 'src/includes/header/header.component';
+import { FooterComponent } from 'src/includes/footer/footer.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, HeaderComponent, FooterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
