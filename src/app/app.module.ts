17cfd4786934016fa487dcb349aad3a586e4c41a
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-30 16:49:13 +0530

    working on main component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+import { MainComponent } from './pages/main/main.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatModule } from './appModules/mat.module';
-import { HeaderComponent } from 'src/includes/header/header.component';
-import { FooterComponent } from 'src/includes/footer/footer.component';
+import { HeaderComponent } from './includes/header/header.component';
+import { FooterComponent } from './includes/footer/footer.component';
+
+const appRoutes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/appModules/mat.module.ts
+++ src/app/appModules/mat.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
-const material = [MatToolbarModule, MatIconModule, MatButtonModule, MatListModule];
+const material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule,
+  MatCardModule,
+];
 
 @NgModule({
   imports: [CommonModule, material],
